05df278de73c990405c2a08829769f1fa014b4a7
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-05 09:57:18 -0500

    Use @AfterReturning advice to log value returned by methods intercepted by LoggingAspect

--- src/main/java/aspects/LoggingAspect.java
+++ src/main/java/aspects/LoggingAspect.java
@@ -1,10 +1,8 @@
 package aspects;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 
-import java.util.Arrays;
 import java.util.logging.Logger;
 
 @Aspect
@@ -12,19 +10,8 @@ public class LoggingAspect {
 
     private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
 
-    @Around("@annotation(ToLog)")
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        String methodName = joinPoint.getSignature().getName();
-        Object[] arguments = joinPoint.getArgs();
-
-        logger.info("Method " + methodName +
-                " with parameters " + Arrays.asList(arguments) +
-                " will execute");
-
-        Object returnedByMethod = joinPoint.proceed();
-
-        logger.info("Method executed and returned " + returnedByMethod);
-
-        return returnedByMethod;
+    @AfterReturning(value = "@annotation(ToLog)", returning = "returnedValue")
+    public void log(Object returnedValue) {
+        logger.info("Method executed and returned " + returnedValue);
     }
 }
