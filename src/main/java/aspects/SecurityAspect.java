255aab8e2fa55154484ec0a75238030a6b85b709
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-06 18:51:45 -0500

    Use @Order annotation to define an order for the aspects to execute

--- src/main/java/aspects/SecurityAspect.java
+++ src/main/java/aspects/SecurityAspect.java
@@ -3,10 +3,12 @@ package aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 
 import java.util.logging.Logger;
 
 @Aspect
+@Order(1)
 public class SecurityAspect {
 
     private Logger logger = Logger.getLogger(SecurityAspect.class.getName());
